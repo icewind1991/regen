36fb54ae4bebdffa773deab4cc363b58c1af54d7
Robin Appelman <icewind@owncloud.com>
2015-07-15 19:49:20 +0200

    split off break transformer

--- lib/Transformer/Generator/BreakStatementGroupTransformer.php
+++ lib/Transformer/Generator/BreakStatementGroupTransformer.php
@@ -8,41 +8,60 @@ class BreakStatementGroupTransformer extends StatementGroupTransformer {
 	/**
 	 * @param StatementGroup $group
 	 * @return array [StatementGroup[] $doneGroups, StatementGroup[] $newGroups]
-	 * @throws \Exception
 	 */
 	public function transformGroup(StatementGroup $group) {
 		$statement = array_pop($group->statements);
 
 		if ($statement instanceof Node\Stmt\Break_) {
-			if ($statement->num) {
-				if ($statement->num instanceof Node\Scalar\LNumber) {
-					$count = $statement->num->value;
-				} else {
-					throw new \Exception('only constant integer breaks are supported');
-				}
-			} else {
-				$count = 1;
-			}
-			$activeGroup = $group->parent;
-			while ($activeGroup && ($count > 0)) {
-				if (
-					$activeGroup instanceof WhileStatementGroup
-				) {
-					$count--;
-				}
-				if ($count > 0) {
-					if (!$activeGroup->parent) {
-						throw new \Exception('invalid break statement');
-					}
-					$activeGroup = $activeGroup->parent;
-				}
-			}
-			array_push($group->statements, $this->getGroupEndCall($activeGroup));
+			$count = $this->getBreakCount($statement);
+			$targetGroup = $this->getTargetGroup($group, $count);
+			array_push($group->statements, $this->getGroupEndCall($targetGroup));
 		}
 
 		return [
 			[$group],
 			[]
 		];
+	}
+
+	/**
+	 * @param Node\Stmt\Break_ $statement
+	 * @return int
+	 * @throws \Exception
+	 */
+	protected function getBreakCount(Node\Stmt\Break_ $statement) {
+		if ($statement->num) {
+			if ($statement->num instanceof Node\Scalar\LNumber) {
+				return $statement->num->value;
+			} else {
+				throw new \Exception('only constant integer breaks are supported');
+			}
+		} else {
+			return 1;
+		}
+	}
+
+	/**
+	 * @param StatementGroup $group
+	 * @param int $count
+	 * @return StatementGroup
+	 * @throws \Exception
+	 */
+	protected function getTargetGroup(StatementGroup $group, $count) {
+		$activeGroup = $group->parent;
+		while ($activeGroup && ($count > 0)) {
+			if (
+				$activeGroup instanceof WhileStatementGroup
+			) {
+				$count--;
+			}
+			if ($count > 0) {
+				if (!$activeGroup->parent) {
+					throw new \Exception('invalid break statement');
+				}
+				$activeGroup = $activeGroup->parent;
+			}
+		}
+		return $activeGroup;
 	}
 }
