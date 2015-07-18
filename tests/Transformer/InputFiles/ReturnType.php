94024d3ef63007c9dcf81f76d8683f36def7fb04
Robin Appelman <icewind@owncloud.com>
2015-07-18 17:56:59 +0200

    add return type tests

---
+++ tests/Transformer/InputFiles/ReturnType.php
@@ -0,0 +1,5 @@
+<?php
+
+return function ($a):string {
+	return $a;
+};
---
+++ tests/Transformer/TypeHintTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Regen\Tests\Transformer;
+
+use Regen\Tests\Polyfill\OperatorTest;
+use Regen\Transformer\ArgumentUnpackingVisitor;
+use Regen\Transformer\Operators;
+use Regen\Transformer\ReturnTypeVisitor;
+use Regen\Transformer\TypeHint;
+use Regen\Transformer\VariadicFunctionVisitor;
+
+class TypeHintTest extends VisitorTest {
+	public function testReturnTypeCompare() {
+		$this->skipIfVersionLowerThan('7.0.0');
+		$code = file_get_contents(__DIR__ . '/InputFiles/ReturnType.php');
+		$this->assertBeforeAndAfter(
+			[$this->visitorFromTransformer(new TypeHint())],
+			$code,
+			['a']
+		);
+
+		try {
+			$this->assertBeforeAndAfter(
+				[$this->visitorFromTransformer(new TypeHint())],
+				$code,
+				[1]
+			);
+			$this->fail('Expected return type error');
+		} catch (\TypeError $e) {
+			$this->assertTrue(true);
+		}
+	}
+
+	public function testReturnType() {
+		$code = file_get_contents(__DIR__ . '/InputFiles/ReturnType.php');
+		$this->assertCodeResult(
+			[$this->visitorFromTransformer(new TypeHint())],
+			$code,
+			['a'],
+			'a'
+		);
+
+		try {
+			$this->assertCodeResult(
+				[$this->visitorFromTransformer(new TypeHint())],
+				$code,
+				[1],
+				null
+			);
+		} catch (\TypeError $e) {
+			$this->assertTrue(true);
+		}
+	}
+}
